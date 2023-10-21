9b57daca0e38b6db646de6d6f86b083ea79a40dd
Jans110718 <jansyaredjade111@gmail.com>
2023-10-21 12:57:24 -0500

    resuelto

--- src/main/java/com/empresa/repository/ProveedorRepository.java
+++ src/main/java/com/empresa/repository/ProveedorRepository.java
@@ -1,10 +1,17 @@
 package com.empresa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import com.empresa.entity.Proveedor;
 
 public interface ProveedorRepository extends JpaRepository<Proveedor, Integer>{
+
 	
 
+	
+	
 }
